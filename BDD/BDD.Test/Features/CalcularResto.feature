0b74fbcf3e940a4a3802aeb17c3c4ddb5a5be368
pauloEzequiel <flossgroup2021@gmail.com>
2022-09-07 23:13:25 -0300

    Adição de teste com calculadora de resto

---
+++ BDD/BDD.Test/Features/CalcularResto.feature
@@ -0,0 +1,12 @@
+﻿#language: pt-br
+
+Funcionalidade: CalcularResto
+
+
+Cenário: Calcular resto entre dois numeros
+	Dado que naveguei para pagina da calculadora de resto
+	E que o valor inserindo no dividendo é 20
+    E que o valor inserido no divisor é 10 
+	Quando ao um clicar em calcular resto
+	Entao o resultado do resto é 0 
+	E o resultado da divisão é 2
---
+++ BDD/BDD.Test/Steps/CalcularRestoSteps.cs
@@ -0,0 +1,88 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace BDD.Test.Steps
+{
+    [Binding]
+    public class CalcularRestoSteps
+    {
+        IWebDriver Browser;
+
+        [BeforeScenario]
+        public void CreateWebDriver()
+        {
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService("C:\\Users\\paulo\\OneDrive\\Documentos\\BDD_Labs");
+
+            ChromeOptions chromeOpts = new ChromeOptions();
+            chromeOpts.AddArgument("--headless");
+
+            //Cria a instancia do browser antes de executar os cenarios
+            Browser = new ChromeDriver(service, chromeOpts);
+
+
+        }
+
+        [AfterScenario]
+        public void CloseWebDriver()
+        {
+            this.Browser.Close();
+            this.Browser.Dispose();
+        }
+
+        [Given(@"que naveguei para pagina da calculadora de resto")]
+        public void DadoQueNavegueiParaPaginaDaCalculadoraDeResto()
+        {
+            Browser.Navigate().GoToUrl("https://www.4devs.com.br/resto_da_divisao");
+        }
+
+        [Given(@"que o valor inserindo no dividendo é (.*)")]
+        public void DadoQueOValorInserindoNoDividendoE(int num)
+        {
+            var dividendo = Browser.FindElement(By.Id("num1"));
+            dividendo.SendKeys(num.ToString());
+           
+        }
+
+        [Given(@"que o valor inserido no divisor é (.*)")]
+        public void DadoQueOValorInseridoNoDivisorE(int num)
+        {
+            var divisor = Browser.FindElement(By.Id("num2"));
+            divisor.SendKeys(num.ToString());
+        }
+
+        [When(@"ao um clicar em calcular resto")]
+        public void QuandoAoUmClicarEmCalcularResto()
+        {
+            var botao = Browser.FindElement(By.Id("bt_calcular"));
+            botao.Click();
+        }
+
+        [Then(@"o resultado do resto é (.*)")]
+        public void EntaoOResultadoDoRestoE(int num)
+        {
+            var resposta = Browser.FindElement(By.Id("texto_resposta")).Text;
+            var respostaResto = resposta.Split("\n")[0];
+            var resto = respostaResto.Substring(respostaResto.LastIndexOf('=')+1);
+
+
+            Assert.True(resto == num.ToString());
+        }
+
+        [Then(@"o resultado da divisão é (.*)")]
+        public void EntaoOResultadoDaDivisaoE(int num)
+        {
+            var resposta = Browser.FindElement(By.Id("texto_resposta")).Text;
+            var respostaTratada = resposta.Split("\n")[1];
+            var resultado = respostaTratada.Substring(respostaTratada.LastIndexOf('=') + 1);
+
+
+            Assert.True(resultado == num.ToString());
+        }
+
+
+    }
+}
